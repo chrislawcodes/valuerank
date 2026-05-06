2d5fdf0e10c29a28a9c7d04f4091e79ec72e12a0
Chris Law <chrislaw@gmail.com>
2026-05-06 10:36:28 -0700

    Align confidence report averages

--- cloud/apps/web/src/pages/ModelsConfidence.tsx
+++ cloud/apps/web/src/pages/ModelsConfidence.tsx
@@ -205,6 +205,7 @@ export function ModelsConfidence() {
       {domains.length > 0 && (
         <ConfidenceModelDomainBreakout
           domains={domains}
+          referenceModels={models}
           signature={selectedSignature}
           selectedModelIds={selectedModelIds}
           defaultModelIds={defaultModelIds}
